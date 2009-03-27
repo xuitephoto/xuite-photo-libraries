01e8a360eb93b1f2e7b5e8c2d03725324524c7e8
xuitephoto <ericsk@cht.com.tw>
2009-03-27 13:43:57 +0800

    Add ruby client library

---
+++ ruby/xuite/photo.rb
@@ -0,0 +1,99 @@
+#
+# = xuite/photo.rb - Xuite Photo Service Client
+#
+# Copyright 2009 Xuite Photo
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+module Xuite
+
+    class Photo
+        private_class_method :new    
+
+        SERVICE_ENDPOINT_URL = 'http://photo.xuite.net/_service/xmlrpc'
+
+        @@instance = nil
+        @@public_key = ''
+        @@private_key = ''
+
+        # get xuite photo service instance
+        def self.get_service()
+            @@instance = new unless @@instance
+            @@instance
+        end
+
+        def self.create_signature(method_name, params)
+            require 'digest/sha2'
+            
+            params['api_key'] = @@public_key if not params.include? 'api_key'
+            params['method'] = method_name if not params.include? 'method'
+
+            raw_string = @@private_key
+            params.keys.sort.each do |key|
+                raw_string += key + params[key].to_s
+            end
+            params['api_sig'] = Digest::SHA256.hexdigest(ras_string)
+            params.delete('method')
+        end
+
+
+        def create_album(title = '', desc = '', auth_token = '')
+            self._invoke_method('xuite.photo.album.create', {
+                'title' => title,
+                'desc' => desc,
+                'auth_token' => auth_token
+            })
+        end
+
+        def get_frob()
+            self._invoke_method('xuite.photo.auth.getFrob')
+        end
+
+        def get_token(frob)
+            self._invoke_method('xuite.photo.auth.getToken', {'frob' => frob})
+        end
+
+        def get_albums(auth_token)
+            self._invoke_method('xuite.photo.user.getAlbums', {'auth_token' => auth_token})
+        end
+
+        def get_quota(auth_token)
+            self._invoke_method('xuite.photo.user.getQuota', {'auth_token' => auth_token})
+        end
+
+        private
+        def initialize()
+            
+        end
+
+        def _invoke_method(method_name, params = {})
+            require 'xmlrpc/client'
+            
+            Xuite::Photo.create_signature(method_name, params)
+            
+            xuite = XMLRPC::Client.new2(SERVICE_ENDPOINT_URL)
+            xuite.call(method_name, params)
+        end
+    end
+
+end

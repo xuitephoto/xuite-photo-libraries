b4243de6d19156dab80708b6ac02ba40801ecba1
xuitephoto <ericsk@cht.com.tw>
2009-04-03 09:19:19 +0800

    fix python library

--- python/xuite/photo.py
+++ python/xuite/photo.py
@@ -23,50 +23,49 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
-
+ 
 """ The xuite photo package. """
-
+ 
 SERVICE_ENDPOINT_URL = 'http://photo.xuite.net/_service/xmlrpc'
-
+ 
 def create_signature(method_name, params, **key):
     """
         create_signature is used to hash the parameters (and method name)
         to be an API signature for API invoking.
-
+ 
         This method will modify the content of params
     """
     import hashlib
-
+ 
     public_key = key.get('public_key', '')
     private_key = key.get('private_key', '')
-
+ 
     if not params.has_key('api_key'):
         params['api_key'] = public_key
-
+ 
     if not params.has_key('method'):
         params['method'] = method_name
-
+ 
     # TODO: sort and combine
     sorted_keys = sorted(params.keys())
-    raw = []
+    raw = [private_key]
     for k in sorted_keys:
         raw.append('%s%s' % (k, params[k]))
-
     hash = hashlib.sha256()
     hash.update(''.join(raw))
     params['api_sig'] = hash.hexdigest()
-
+ 
     del params['method']
-
-
+ 
+ 
 class ServiceMethodNotFoundError(Exception):
     def __init__(self, message):
         self.message = message
-
+ 
     def __str__(self):
         return repr(self.message)
-
-
+ 
+ 
 class XuitePhotoService(object):
     class __impl:
         """
@@ -75,63 +74,65 @@ class XuitePhotoService(object):
         def __init__(self, **keys):
             self.public_key = keys.get('public_key')
             self.private_key = keys.get('private_key')
-
+ 
         def create_album(self, auth_token, **args):
             """
                 The wrapper of xuite.photo.album.create
             """
-            title = args.get('title')
-            desc = args.get('desc')
-
+            title = args.get('title', '')
+            desc = args.get('desc', '')
+ 
             params = {'auth_token': auth_token}
-            params['title'] = title if title is not None
-            params['desc'] = desc if desc is not None
-
+            params.update({
+                'title': title,
+                'desc': desc
+            })
+ 
             return self._invoke('xuite.photo.album.create', params)
             
-
+ 
         def get_frob(self):
             """
                 The wrapper of xuite.photo.auth.getFrob
             """
-            return self._invoke('xuite.photo.auth.getFrob')
-
+            return self._invoke('xuite.photo.auth.getFrob', {})
+ 
         def get_token(self, frob):
             """
                 The wrapper of xuite.photo.auth.getToken
             """
             return self._invoke('xuite.photo.auth.getToken', {'frob': frob})
-
+ 
         def get_albums(self, auth_token):
             """
                 The wrapper of xuite.photo.user.getAlbums
             """
             return self._invoke('xuite.photo.user.getAlbums', {'auth_token': auth_token})
-
+ 
         def get_quota(self, auth_token):
             """
                 The wrapper of xuite.photo.user.getQuota
             """
             return self._invoke('xuite.photo.user.getQuota', {'auth_token': auth_token})
-
+ 
         def _invoke(self, method_name, params):
             import xmlrpclib
-
+ 
             create_signature(method_name, params, 
                             public_key=self.public_key, private_key=self.private_key)
             xmlrpc_proxy = xmlrpclib.ServerProxy(SERVICE_ENDPOINT_URL, encoding='utf-8')
-
+            print params
             response = None
             try:
                 # invoking the api method
                 response = getattr(xmlrpc_proxy, method_name)(params)
             except AttributeError: # method not found
                 raise ServiceMethodNotFoundError('%s: method not found' % method_name)
-
+ 
             return response
-
+ 
     __instance = None
-
+ 
     def __init__(self, **keys):
         """
         Singleton method.
@@ -142,9 +143,10 @@ class XuitePhotoService(object):
             XuitePhotoService.__instance = XuitePhotoService.__impl(public_key=public_key,
                                                                     private_key=private_key)
         self.__dict__['_XuitePhotoService__instance'] = XuitePhotoService.__instance
-
+ 
     def __getattr__(self, attr):
         return getattr(self.__instance, attr)
-
+ 
     def __setattr__(self, attr, value):
         return setattr(self.__instance, attr, value)
+
